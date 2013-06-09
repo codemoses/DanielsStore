3654c2e2cc297796d122b1a9e6de419b1468d6ac
Moses W. Jerao <mjerao@rrc.ca>
2013-06-09 16:05:39 -0500

    Added website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DanielsStore::Application.config.secret_token = '4d98bfe20dffff565c98622135751c4132015cea0769c7f26d8beda672fe9b4664ed7631765811db2572257b67fdf1481d8fa7e677003be6494b702d19bc9476'
